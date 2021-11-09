8c494fab9c2d61aa3b0215278e46060268dcbc47
Enzo-Bellini <bellini.enzo13@gmail.com>
2021-11-09 20:33:22 -0300

    rebase

--- src/Componentes/Contato/Contato.jsx
+++ src/Componentes/Contato/Contato.jsx
@@ -4,126 +4,45 @@ import Enzo from "../../Assets/img/enzo-2.jpg";
 import Dudu from "../../Assets/img/dudu.jpg";
 import Joao from "../../Assets/img/joao-3.jpg";
 import Teixeira from "../../Assets/img/duduteixeira.jpg";
-import Github from "../../Assets/img/github.jpg" ;
-import Instagram from "../../Assets/img/instagram.jpg";
-import Linkedin from "../../Assets/img/linkedin.jpg";
-import Smartphone from "../../Assets/img/smartphone.svg";
+import Lista_1 from "./Lista";
+import Lista_2 from "./Lista";
+import Lista_3 from "./Lista";
+import Lista_4 from "./Lista";
 
-
+ 
 
 class Contato extends Component {
+   
+  
+   
     render() {
         return (
         <section className = "contato-section">    
+            <h1 className = "contato-titulo">Contatos</h1>
+
             <ul>
-            <li>
+            <li className = "contato-li">
             <img src = {Enzo} alt = "enzo" className = "perfil"/>
-            <p className = "nome">Enzo R. Bellini</p>
-       <details className = "Contato-details">
-            <ul  className = "Contato-lista">
-                <li>
-                    <img src = {Smartphone} alt = "telefone " className = "celular"/><p>(51) 99141-5252</p>
-                </li>
-
-                <li>
-                    <a href="https://github.com/EnzoRolimBellini" target="_blank">
-                        <img src = {Github} alt = "github" className = "Contato-icone"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="" target="_blank">
-                        <img src = {Linkedin} alt = "linkedin" className = "Contato-icone"/>
-                    </a>
-                </li>
-
-            </ul>
-           </details>
+            <p className = "nome" >Enzo R. Bellini</p>
+                <Lista_1/>
             </li>
 
             <li>
             <img src = {Dudu} alt = "dudu" className = "perfil"/>
             <p className = "nome">Eduardo G. Fensterseifer</p>
-            <details className = "Contato-details">   
-            <ul className = "Contato-lista">
-                <li>
-                    <img src = {Smartphone} alt = "telefone " className = "celular"/><p>(51) 98114-1589</p>
-                </li>
-
-                <li>
-                    <a href="https://github.com/EduardoFen12" target="_blank">
-                        <img src = {Github} alt = "github" className = "Contato-icone"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/eduardo-fensterseifer" target="_blank">
-                        <img src = {Linkedin} alt = "linkedin" className = "Contato-icone"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/eduardo_fensterseifer/" target="_blank">
-                        <img src= {Instagram} alt = "instagram" className = "Contato-icone"/>
-                    </a>
-                </li>
-            </ul>
-            </details>
+                <Lista_2/>
             </li>
 
             <li>
             <img src = {Joao} alt = "joao" className = "perfil"/> 
             <p className = "nome" >João Pedro Rodrigues</p>
-            <details className = "Contato-details">
-            <div className = "Contato-dados">    
-                <ul  className = "Contato-lista">
-                    <li>
-                        <img src = {Smartphone} alt = "telefone " className = "celular"/> <p>(54) 98436-9987</p>
-                    </li>
-
-                     <li>
-                        <a href="https://github.com/Joao-Pedro2804" target="_blank">
-                            <img src = {Github} alt = "github"  className = "Contato-icone"/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/joao-pedro-rodri/" target="_blank">
-                            <img src = {Linkedin} alt = "linkedin"  className = "Contato-icone"/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/joaoprodri_/" target="_blank">
-                            <img src= {Instagram} alt = "instagram"  className = "Contato-icone"/>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            </details> 
+                <Lista_3/>
             </li>
 
             <li>           
             <img src = {Teixeira} alt = "eduardo teixeira" className = "perfil"/>    
             <p className = "nome">Eduardo C. Teixeira</p>
-            <details className = "Contato-details">
-            <ul  className = "Contato-lista">
-                <li>
-                    <img src = {Smartphone} alt = "telefone " className = "celular"/><p> (51) 98619-6898</p>
-                </li>
-
-                <li>
-                    <a href="https://github.com/Texcon38" target="_blank"> 
-                        <img src = {Github} alt = "github" className = "Contato-icone"/>    
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/eduardo-constante-teixeira-716222210" target="_blank">
-                        <img src = {Linkedin} alt = "linkedin"  className = "Contato-icone"/>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/eduardoconstantee_/" target="_blank">
-                        <img src= {Instagram} alt = "instagram"  className = "Contato-icone"/>      
-                    </a>
-                </li>
-            </ul>
-            </details>
+                <Lista_4/>
             </li>
         </ul>
       </section>  
---
+++ src/Componentes/Contato/Lista.jsx
@@ -0,0 +1,134 @@
+import React, {Component} from "react";
+import Github from "../../Assets/img/github.jpg" 
+import Linkedin from "../../Assets/img/linkedin.jpg";
+import Smartphone from "../../Assets/img/smartphone.svg";
+import Instagram from "../../Assets/img/instagram.jpg";
+import "./Contato.css";
+
+
+
+ export default class Lista_1 extends Component { 
+    
+    
+    render(){ 
+        return (    
+        <details className = "Contato-details">
+        <ul  className = "Contato-lista">
+        <li>
+            <img src = {Smartphone} alt = "telefone " className = "celular" /><p>(51) 99141-5252</p>
+        </li>
+
+        <li>
+            <a href="https://github.com/EnzoRolimBellini" target="_blank">
+                <img src = {Github} alt = "github" className = "Contato-icone"/>
+            </a>
+        </li> 
+
+        <li>
+            <a href="" target="_blank">
+                <img src = {Linkedin} alt = "linkedin" className = "Contato-icone"/>
+            </a>
+        </li>
+
+        </ul>
+        </details>
+        )    
+    }
+}    
+
+
+ export class Lista_2 {
+    render(){
+        return (
+            <details className = "Contato-details">   
+            <ul className = "Contato-lista">
+                <li>
+                    <img src = {Smartphone} alt = "telefone " className = "celular"/><p>(51) 98114-1589</p>
+                </li>
+
+                <li>
+                    <a href="https://github.com/EduardoFen12" target="_blank">
+                        <img src = {Github} alt = "github" className = "Contato-icone"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/eduardo-fensterseifer" target="_blank">
+                        <img src = {Linkedin} alt = "linkedin" className = "Contato-icone"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/eduardo_fensterseifer/" target="_blank">
+                        <img src= {Instagram} alt = "instagram" className = "Contato-icone"/>
+                    </a>
+                </li>
+            </ul>
+            </details>
+        )
+    }
+}
+
+
+ export class Lista_3 {
+    render(){
+        return (
+            <details className = "Contato-details"> 
+                <ul  className = "Contato-lista">
+                    <li>
+                        <img src = {Smartphone} alt = "telefone " className = "celular"/> <p>(54) 98436-9987</p>
+                    </li>
+
+                     <li>
+                        <a href="https://github.com/Joao-Pedro2804" target="_blank">
+                            <img src = {Github} alt = "github"  className = "Contato-icone"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/joao-pedro-rodri/" target="_blank">
+                            <img src = {Linkedin} alt = "linkedin"  className = "Contato-icone"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/joaoprodri_/" target="_blank">
+                            <img src= {Instagram} alt = "instagram"  className = "Contato-icone"/>
+                        </a>
+                    </li>
+                </ul>
+            </details> 
+        )
+    }
+}
+
+
+
+ export class Lista_4 {
+    render(){
+        return(
+            <details className = "Contato-details">
+            <ul  className = "Contato-lista">
+                <li>
+                    <img src = {Smartphone} alt = "telefone " className = "celular"/><p> (51) 98619-6898</p>
+                </li>
+
+                <li>
+                    <a href="https://github.com/Texcon38" target="_blank"> 
+                        <img src = {Github} alt = "github" className = "Contato-icone"/>    
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/eduardo-constante-teixeira-716222210" target="_blank">
+                        <img src = {Linkedin} alt = "linkedin"  className = "Contato-icone"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/eduardoconstantee_/" target="_blank">
+                        <img src= {Instagram} alt = "instagram"  className = "Contato-icone"/>      
+                    </a>
+                </li>
+            </ul>
+            </details>
+        )
+    }
+}
+
+
+
